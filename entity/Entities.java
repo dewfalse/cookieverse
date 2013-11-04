aa2c7b69099d9d840fafaf7205879fa7174a789c
dewfalse <dewfalse@gmail.com>
2013-11-04 17:49:37 +0900

    Entityの名前がローカライズされない問題を修正

--- entity/Entities.java
+++ entity/Entities.java
@@ -12,12 +12,12 @@ public class Entities {
 
 
 	public static void init() {
-		LanguageRegistry.instance().addStringLocalization("entity.Cocow.name", "en_US", "Cocow");
-		LanguageRegistry.instance().addStringLocalization("entity.Sughast.name", "en_US", "Sughast");
-		LanguageRegistry.instance().addStringLocalization("entity.Milkreeper.name", "en_US", "Milkreeper");
-		LanguageRegistry.instance().addStringLocalization("entity.Cocow.name", "ja_JP", "カカウ");
-		LanguageRegistry.instance().addStringLocalization("entity.Sughast.name", "ja_JP", "シュガスト");
-		LanguageRegistry.instance().addStringLocalization("entity.Milkreeper.name", "ja_JP", "ミルクリーパー");
+		LanguageRegistry.instance().addStringLocalization("entity.cookieverse.Cocow.name", "en_US", "Cocow");
+		LanguageRegistry.instance().addStringLocalization("entity.cookieverse.Sughast.name", "en_US", "Sughast");
+		LanguageRegistry.instance().addStringLocalization("entity.cookieverse.Milkreeper.name", "en_US", "Milkreeper");
+		LanguageRegistry.instance().addStringLocalization("entity.cookieverse.Cocow.name", "ja_JP", "カカウ");
+		LanguageRegistry.instance().addStringLocalization("entity.cookieverse.Sughast.name", "ja_JP", "シュガスト");
+		LanguageRegistry.instance().addStringLocalization("entity.cookieverse.Milkreeper.name", "ja_JP", "ミルクリーパー");
 		EntityRegistry.registerModEntity(EntityCocow.class, "Cocow", 0, Cookieverse.instance, 64, 2, true);
 		EntityRegistry.registerModEntity(EntitySughast.class, "Sughast", 1, Cookieverse.instance, 64, 2, true);
 		EntityRegistry.registerModEntity(EntityMilkreeper.class, "Milkreeper", 2, Cookieverse.instance, 64, 2, true);
